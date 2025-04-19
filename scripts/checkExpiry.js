d47c494a2657b3224564ee394ea46d7f3c2d578e
Rishab Das <dasrishab3846@gmail.com>
2025-04-19 15:14:57 +0530

    fix expiryCheck

--- scripts/checkExpiry.js
+++ scripts/checkExpiry.js
@@ -1,4 +1,6 @@
 const fetch = require("node-fetch");
+const dotenv = require("dotenv");
+dotenv.config();
 
 async function isExpired() {
   const daysBefore3 = new Date(Date.now() - 4 * 86400 * 1000).toISOString();
@@ -7,7 +9,7 @@ async function isExpired() {
     const response = await fetch(process.env.expiryFileUrl);
     if (!response.ok) throw new Error(`Blob fetch failed: ${response.status}`);
     const lastFetchTime = await response.text();
-    console.log("Expiry Check Successful");
+    console.log("Expiry Check Successful", lastFetchTime);
     return new Date(lastFetchTime) < new Date(daysBefore3);
   } catch (err) {
     console.log(err);
